ce1fbd5325456aed71dbcee501e1c0c26ae04388
Sonr4 <ciele39@gmail.com>
2025-06-12 16:09:08 -0300

    Adding Authentication Support

---
+++ 15_RestWithASPNETUdemy_Authentication/RestWithASPNETUdemy/Repository/Generic/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNETErudio.Model.Base;
+
+namespace RestWithASPNETErudio.Repository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create(T item);
+        T FindByID(long id);
+        List<T> FindAll();
+        T Update(T item);
+        void Delete(long id);
+        bool Exists(long id);
+    }
+}
