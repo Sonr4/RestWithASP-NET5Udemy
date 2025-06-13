fa1d9bd13542825f3c99435628882e977f0b285c
Sonr4 <ciele39@gmail.com>
2025-06-13 14:19:56 -0300

    Working with PATCH Verb

---
+++ 16_RestWithASPNETUdemy_PATHVerb/RestWithASPNETUdemy/Repository/IPersonRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETErudio.Model;
+using RestWithASPNETErudio.Repository;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Repository
+{
+    public interface IPersonRepository : IPersonRepository<Person>
+    {
+        Person Disable(long id);
+    }
+}
